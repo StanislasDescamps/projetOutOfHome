61e102a7006a8cd332981ec91fa480a553365741
Stanislas <stanislas.descamps@gmail.com>
2015-05-04 22:52:53 -0400

    Création d'un code de suppression des tables pour plus de rapidité

---
+++ src/main/sql/suppressionTables.sql
@@ -0,0 +1,7 @@
+DROP TABLE `doubletvote`;
+DROP TABLE `doubletgenre`;
+DROP TABLE `doubletlangue`;
+DROP TABLE `genreactivite`;
+DROP TABLE `activite`;
+DROP TABLE `utilisateur`;
+DROP TABLE `langues`;
