c2acd142a3146b3d17f564310109efb4494be29e
Stanislas <stanislas.descamps@gmail.com>
2015-05-04 22:51:01 -0400

    Retrait du code

--- src/main/java/controllers/AccueilServlet.java
+++ src/main/java/controllers/AccueilServlet.java
@@ -26,57 +26,7 @@ public class AccueilServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		/*Integer idUtilisateur=Integer.parseInt(request.getParameter("idUtilisateur"));
 		
-		//Recuperation de tous les utilisateur (-l'utilisateurActuel)
-		Utilisateur utilisateurActuel=Manager.getInstance().getUtilisateur(idUtilisateur);	
-		List<Utilisateur> toutUtilisateur=Manager.getInstance().listerUtilisateur();
-		toutUtilisateur.remove(utilisateurActuel);
-		
-		//liste des utilisateur environnant 
-		List<Utilisateur> listeUtilisateurArround=new ArrayList<Utilisateur>();
-		Integer rayon=40000; //distance du rayon en m
-		
-		//adapter le code suivant la geolocalisation
-		for(Utilisateur utilisateur : toutUtilisateur){
-			if(distanceTo(convert(utilisateur.getPosition())) <= rayon){  //voir comment ca marche
-				listeUtilisateurArround.add(utilisateur);
-			}
-		}
-		
-		
-		//Initialisation de la liste finale
-		List<Utilisateur> newListeUtilisateurArround=new ArrayList<Utilisateur>();
-		
-		//voir si dans cette nouvelle liste il y a quelqu'un parlant la meme langue
-		if(listeUtilisateurArround!=null){
-			//Recuperation des langues de l'utilisateur actuel
-			List<Langue> listeLangueUtilisateurActuel=Manager.getInstance().listerLangueByUtilisateur(idUtilisateur);
-			
-			for(Utilisateur utilisateur2 : listeUtilisateurArround){
-				//Recuperation des langues de l'utilisateur lambda
-				List<Langue> listeLangue=Manager.getInstance().listerLangueByUtilisateur(utilisateur2.getIdUtilisateur());
-					
-				//comparaison de la liste de langue de l'utilisateur actuel et du lambda
-					for(Langue langue: listeLangueUtilisateurActuel ){
-						if(listeLangue.contains(langue)){
-							newListeUtilisateurArround.add(utilisateur2);
-						}
-					}
-			}
-		}
-    	
-		Gson gson1 = new Gson();
-		Gson gson2 = new Gson();
-		
-		String pos="geolocalisation"; //comment qu'on fait?
-		
-		String json1 = gson1.toJson(Manager.getInstance().listerActiviteByLieu(pos));
-		String json2 = gson2.toJson(newListeUtilisateurArround);
-		
-		PrintWriter out = response.getWriter();
-		out.append(json1);
-		out.append(json2);*/
 		
 		 //Permet l'ouverture de la page
     	RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/pages/accueilTempo.jsp");
